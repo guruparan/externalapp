7948a6a76583a7b7dca79545bee9c35434a07fa6
guruparan <guru_sig@yahoo.com>
2021-11-21 00:13:27 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express();
+const port = 8000;
+var session = require('express-session');
+var jwt_decode = require("jwt-decode");
+app.use(express.json())
+
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 } }));
+
+
+app.use('/', express.static('static'))
+
+app.get('/token', (req, res) => {
+  res.send(req.session.auth);
+});
+
+app.get('/callback', (req, res) => {
+
+  //var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+  var decoded = jwt_decode(req.query.key);
+
+  console.log(JSON.stringify(decoded));
+  req.session.auth = decoded;
+  res.redirect('/')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
+
+//data:text/html,<body onload="document.body.firstChild.submit()"><form method="post" action="http://localhost:8000/callback"><input value="hugh.mahn@person.com" name="auth"></form></body>
