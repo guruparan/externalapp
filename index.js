eaf4f486eef967e65974f25db722ea43475887a2
guruparan <guru_sig@yahoo.com>
2021-11-21 12:09:57 +0530

    Updated apis

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const port = process.env.PORT ||  8000;
+const port = process.env.PORT || 8000;
 var session = require('express-session');
 var jwt_decode = require("jwt-decode");
 app.use(express.json())
@@ -24,7 +24,86 @@ app.get('/callback', (req, res) => {
   res.redirect('/')
 });
 
+app.get('/deletedata/{userId}', (req, res) => {
+  res.send({
+    "result": "success"
+  });
+});
+
+app.get('/deletionstatus', (req, res) => {
+  const responseId = Math.round(Math.random() * 3);
+
+  switch (responseId) {
+    case 0:
+      res.send({
+        "request_id": "234234sdfwer23f3f",
+        "deleted_keys": [
+          "firstName",
+          "lastName"
+        ],
+        "undeleted_keys": [
+          "email",
+          "phone"
+        ],
+        "status": "full",
+        "reason": "Name is displayed in some sections of the app"
+      });
+
+    case 1:
+      res.send({
+        "request_id": "234234sdfwer23f3f",
+        "deleted_keys": [
+          "firstName",
+          "lastName"
+        ],
+        "undeleted_keys": [
+          "email",
+          "phone"
+        ],
+        "status": "full",
+        "reason": "The data is critical to the system."
+      });
+
+    case 2:
+      res.send({
+        "request_id": "234234sdfwer23f3f",
+        "deleted_keys": [
+          "email",
+        ],
+        "undeleted_keys": [
+          "firstName",
+          "lastName",
+          "phone"
+        ],
+        "status": "full",
+        "reason": "Email is as a mapping in our tables"
+      });
+
+    default:
+      res.send({
+        "request_id": "234234sdfwer23f3f",
+        "deleted_keys": [
+          "firstName",
+          "lastName",
+          "email",
+          "phone"
+        ],
+        "undeleted_keys": [
+        ],
+        "status": "full",
+        "reason": "Data deletion completed"
+      });
+  }
+});
+
+app.post('/acknowledge', (req, res) => {
+  //call the IDP endpoint from here
+
+  res.send("Acknowlegement sent");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
+
 
